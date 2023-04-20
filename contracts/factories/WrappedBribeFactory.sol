6ffe615495301d6e6ee9f1fe261f7d901d8c8645
0xmotto <0xmotto@protonmail.com>
2023-04-20 12:38:25 +0800

    fix: add wrapped bribe

---
+++ contracts/factories/WrappedBribeFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {WrappedBribe} from 'contracts/WrappedBribe.sol';
+
+contract WrappedBribeFactory {
+    address public voter;
+    mapping(address => address) public oldBribeToNew;
+    address public last_bribe;
+
+    event VoterSet(address indexed setter, address indexed voter);
+
+    function createBribe(address existing_bribe) external returns (address) {
+        require(
+            oldBribeToNew[existing_bribe] == address(0),
+            "Wrapped bribe already created"
+        );
+        last_bribe = address(new WrappedBribe(voter, existing_bribe));
+        oldBribeToNew[existing_bribe] = last_bribe;
+        return last_bribe;
+    }
+
+    function setVoter(address _voter) external {
+        require(voter == address(0), "Already initialized");
+        voter = _voter;
+        emit VoterSet(msg.sender, _voter);
+    }
+}
